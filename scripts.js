9e979cedda7dff6701d6044167bd961009f29c02
iskezhan <78268546+iskezhan@users.noreply.github.com>
2021-03-02 01:36:12 +0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
+const firstName = document.getElementById("firstname");
+const startingBid = document.getElementById("startingbid");
 const brideEducation = document.querySelector(".bride-education");
 const familyNet = document.querySelector(".family-net");
 const price = document.querySelector(".price");
 const submitQuote = document.getElementById("submit-quote");
+
+const calculate = () => {
+  let name = firstName.value; // name of the groom/bride
+  let price = Number(startingBid.value); // turns your starting bid string into number
+  let letter = love_letter.value;
+  if (name != "") { 
+      price = getNewPrice(price, education);
+      // FINISH THE CODE !!!!!!!!!!!!!!!!
+      let person = {
+          fullName: name,
+          finalPrice: price,
+          loveLetter: letter
+      }
+      document.getElementById("result").innerHTML = `The price for ${person.fullName} is ${person.finalPrice}. Your love letter is ${person.loveLetter}`;
+  }
+  else {
+      alert("Name and starting bid cannot be empty");
+  }
+}
+
 
 var age_prices = new Array();
 age_prices["18"]=1.5;
@@ -10,7 +32,7 @@ age_prices["28"]=0.95;
 
 submitQuote.addEventListener('click', function(){
   
-  //family net worth
+  
   if (familyNet.options[familyNet.selectedIndex].text == "More than 100,000$") {
     answer = 500 * 2;
   } else if (familyNet.options[familyNet.selectedIndex].text == "Between 50,000$ and 100,000$") {
@@ -18,8 +40,8 @@ submitQuote.addEventListener('click', function(){
   } else {
     answer = 500 * 1.2;
   }
-  
-  //education
+
+
   if (brideEducation.options[brideEducation.selectedIndex].text == "Undergraduate degree") {
     answer = answer * 1.5;
   } else if (brideEducation.options[brideEducation.selectedIndex].text == "College degree") {
@@ -32,9 +54,9 @@ submitQuote.addEventListener('click', function(){
     var skills = document.getElementsByTagName("input");
     for (var i = 0; i < 4; i++)
     {
-       if (skills[i].checked)//if the item is checked
+       if (skills[i].checked)
        {
-          answer += (skills[i].value * 1);//then item total is updated to the previous total + value of checked item. Mutplied by 1 so that JS knows that it is a num
+          answer += (skills[i].value * 1);
        }
 
     }
